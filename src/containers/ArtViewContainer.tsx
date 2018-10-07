4b065403b3caf82c336d5d911e4a8e8875542c01
Erick Madduma <emadduma@Arcanys.com>
2018-10-07 17:33:31 +0800

    removed console.log

--- src/containers/ArtViewContainer.tsx
+++ src/containers/ArtViewContainer.tsx
@@ -5,7 +5,6 @@ import * as artsActions from '../actions/artsActions';
 
 function mapStateToProps(state: any, ownProps: any) {
 	const artId = ownProps.match.params.artId;
-	console.log(ownProps)
 	return {
 		artId,
 		art: state.arts.byIds ? state.arts.byIds[artId] || null : null,
