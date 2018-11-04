a0dca2cabdfdfec2064101398775f8620871f94a
John Erick Madduma <madduma.johnerick@gmail.com>
2018-11-04 16:35:25 +0800

    experiment test case

--- src/components/test/Home.spec.tsx
+++ src/components/test/Home.spec.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { shallow } from 'enzyme';
+import sinon from 'sinon';
 
 import * as artsActions from '../../actions/artsActions'
 
@@ -17,6 +18,24 @@ describe('<Home />', () => {
             const spy = jest.spyOn(Home.prototype, 'componentDidMount');
             const component = shallow(<Home arts={arts} artsActions={artsActions}/>);
             expect(spy).toHaveBeenCalled();
+        });
+        it('TEST ------------', () => {
+            const arts: string[] = null;
+            const artsActionStub = {
+                getArtsSuccess: jest.fn(() => true),
+                editArtsSuccess: jest.fn(() => true),
+                getArts: jest.fn(() => {
+                    ['2']
+                }),
+                getArt: jest.fn(() => true)
+            }
+
+            const component = shallow(<Home arts={arts} artsActions={artsActionStub}/>);
+            component.update();
+            console.log(arts)
+            // @ts-ignore
+            expect(component.instance().props.arts).toEqual(['2']);
+            expect(artsActionStub.getArts.mock.calls.length).toBe(1);
         });
     });
 });
